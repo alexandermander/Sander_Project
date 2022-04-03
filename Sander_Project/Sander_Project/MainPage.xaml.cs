2ce2be4c5d25e38c6ce9bcc6601228fc9d81c5bb
alexa <alexa@DESKTOP-E050CSH>
2022-04-03 15:07:09 +0200

    Add project files.

---
+++ Sander_Project/Sander_Project/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sander_Project
+{
+    public partial class MainPage : ContentPage
+	{
+		public string mysting;
+		private string myStringProperty;
+		public int value = 0;
+		public string MyStringProperty
+		{
+			get { return myStringProperty; }
+			set
+			{
+				myStringProperty = value;
+				OnPropertyChanged(nameof(MyStringProperty)); // Notify that there was a change on this property
+			}
+		}
+
+		public MainPage()
+		{
+			InitializeComponent();
+			
+		}
+
+        public void HanderClicked(object sender, EventArgs args)
+        {
+			Console.WriteLine("sander is testing");
+			BindingContext = this;
+			value = value+1;
+			Console.WriteLine(value);
+
+			MyStringProperty = Convert.ToString(value);
+
+		}
+
+      
+    }
+}
